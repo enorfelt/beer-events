202c0cf81f65ec8eddf1c5ac81f76363f5bf0a3f
Erik Norfelt <erik.norfelt@monitor.se>
2025-06-08 08:32:01 +0200

    🌐 added swedish translations

--- src/routes/demo/paraglide/+page.svelte
+++ src/routes/demo/paraglide/+page.svelte
@@ -4,10 +4,10 @@
 	import { goto } from '$app/navigation';
 	import { m } from '$lib/paraglide/messages.js';
 </script>
-
 <h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
 <div>
 	<button onclick={() => setLocale('en')}>en</button>
+	<button onclick={() => setLocale('sv')}>sv</button>
 </div>
 <p>
 	If you use VSCode, install the <a
